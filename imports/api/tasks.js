bc5472ba27889cf49a8e3f4eabd4c61f282c7a6c
Håkansson <HakanssonT@skanska.org>
2016-04-07 12:30:00 +0200

    Fixed bug where owner couldn't change private status for task

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -48,7 +48,7 @@ Meteor.methods({
        
        
        const task = Tasks.findOne(taskId);
-       if (task.private && task.owner !== Meteor.userId) {
+       if (task.private && task.owner !== Meteor.userId()) {
            // If the task is private, make sure only the owner can check it off
            throw new Meteor.Error('not-authorized');
        }
